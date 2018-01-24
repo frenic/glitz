ba82a5b404c2157214f0beb4b87e70c52d93ea9b
Fredrik Nicol <fredrik.nicol@avensia.com>
2018-01-24 22:51:52 +0100

    Convert `Properties` type to a interface

--- packages/type/index.d.ts
+++ packages/type/index.d.ts
@@ -1,17 +1,17 @@
 import * as CSS from 'csstype';
 
-export declare interface Rule extends Properties, PseudoRule {}
+export declare interface Properties extends CSS.PropertiesFallback<string | number> { }
 
-export declare interface Style extends Rule, KeyframesRule {}
+export declare type PseudoRule = {[P in CSS.Pseudos]?: Properties | PseudoRule };
+
+export declare interface Rule extends Properties, PseudoRule { }
 
 export declare interface PropertiesList {
   [identifier: string]: Properties;
 }
 
-export declare type Properties = CSS.PropertiesFallback<string | number>;
+export declare type KeyframesRule = { '@keyframes'?: PropertiesList };
+
+export declare interface Style extends Rule, KeyframesRule { }
 
 export declare type PrimitiveValue = string | number;
-
-export declare type PseudoRule = { [P in CSS.Pseudos]?: Properties | PseudoRule };
-
-export declare type KeyframesRule = { '@keyframes'?: PropertiesList };
