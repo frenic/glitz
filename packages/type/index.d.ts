2d1d4ef9f201dffa27b359a11b8b9ac7bb5ed78e
Fredrik Nicol <fredrik.nicol@gmail.com>
2018-02-19 08:48:36 +0100

    Replace featured CSS properties type

--- packages/type/index.d.ts
+++ packages/type/index.d.ts
@@ -1,15 +1,20 @@
 import * as CSS from 'csstype';
 
-export declare interface Style extends Properties, PseudoMap {
+export interface Style extends FeaturedProperties, PseudoMap {
   '@keyframes'?: PropertiesList;
-  animationName?: PropertiesList | Properties['animationName'];
 }
 
-export declare interface Properties extends CSS.PropertiesFallback {}
+export interface Properties extends CSS.PropertiesFallback {}
 
-export declare interface PropertiesList {
+interface PropertiesList {
   [identifier: string]: Properties;
 }
 
+type FeaturedProperties = { [P in Diff<keyof Properties, 'animationName'>]?: Properties[P] } & {
+  animationName?: PropertiesList | Properties['animationName'];
+};
+
 // Need this due to https://github.com/Microsoft/TypeScript/issues/13573
 type PseudoMap = { [P in CSS.SimplePseudos]?: Style };
+
+type Diff<T extends string, U extends string> = ({ [P in T]: P } & { [P in U]: never } & { [x: string]: never })[T];
