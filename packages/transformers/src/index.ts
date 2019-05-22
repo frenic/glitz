c021cb02060a589fb01c73e83f02704c59d72b2e
Fredrik Nicol <fredrik.nicol@gmail.com>
2019-05-22 15:51:13 +0200

    Remove unnecessary import

--- packages/transformers/src/index.ts
+++ packages/transformers/src/index.ts
@@ -2,8 +2,6 @@ import { compose } from '@glitz/core';
 import { createDevToolTransformer, Options as DevToolOptions } from '@glitz/devtool-transformer';
 import { createNumberToLengthTransformer, Options as NumberToLengthOptions } from '@glitz/length-transformer';
 import prefixer from '@glitz/prefixer-transformer';
-// @ts-ignore
-import { Properties, UntransformedProperties } from '@glitz/type';
 
 export type Options = {
   numberToLengthOptions?: NumberToLengthOptions;
