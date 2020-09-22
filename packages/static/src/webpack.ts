4308dce01a577552c5e6400fda1a5d15bc9c95e0
Fredrik Nicol <fredrik.nicol@gmail.com>
2020-09-22 14:35:41 +0200

    Hash static CSS filenames

--- packages/static/src/webpack.ts
+++ packages/static/src/webpack.ts
@@ -1,21 +1,36 @@
+import * as crypto from 'crypto';
 import { GlitzStatic } from '@glitz/core';
+import type * as webpack from 'webpack';
+
+const PLUGIN_NAME = 'GlitzStaticPlugin';
+const HASH_REGEX = /\[contenthash(?::(\d+))?\]/i;
 
 type Output = ((output: string) => Promise<unknown> | void) | string;
 
 export class GlitzStaticPlugin {
   private glitz: GlitzStatic;
   private output: Output;
+  private hash: (css: string, length: number) => string;
   constructor(glitz: GlitzStatic, output: Output) {
     this.glitz = glitz;
     this.output = output;
+    this.hash = (css, length): string => {
+      const hash = crypto.createHash('md5', { encoding: 'hex' });
+      hash.write(css);
+      hash.end();
+      return hash.read(length);
+    };
   }
-  apply(compiler: any) {
-    compiler.hooks.emit.tapAsync('GlitzStaticPlugin', async (compilation: any, callback: () => void) => {
-      const css = this.glitz.getStyle();
-      const output = this.output;
+  apply(compiler: webpack.Compiler) {
+    const output = this.output;
 
+    compiler.hooks.emit.tapPromise(PLUGIN_NAME, async compilation => {
+      const css = this.glitz.getStyle();
       if (typeof output === 'string') {
-        compilation.assets[output] = {
+        const filename = output.replace(HASH_REGEX, ({}, length) =>
+          this.hash(css, length ? parseInt(length, 10) : Infinity),
+        );
+        compilation.assets[filename] = {
           source() {
             return css;
           },
@@ -26,8 +41,6 @@ export class GlitzStaticPlugin {
       } else {
         await output(css);
       }
-
-      callback();
     });
   }
 }
