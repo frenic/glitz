a2b10220bf0d99f7e74d17f51ebf84b3f1061e93
Fredrik Nicol <fredrik.nicol@gmail.com>
2020-10-02 20:30:26 +0200

    Add cases with stable Glitz

---
+++ packages/benchmarks/cases/hundred-components/stable.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { GlitzClient } from '@glitz/core-stable';
+import { GlitzProvider, styled } from '@glitz/react-stable';
+
+const glitz = new GlitzClient();
+
+function randomRGB() {
+  const random = () => Math.round(Math.random() * 255);
+  return `rgb(${random()}, ${random()}, ${random()})`;
+}
+
+export default function () {
+  const components: any[] = [];
+  for (let i = 0; i < 100; i++) {
+    components.push(
+      styled.div({
+        backgroundColor: randomRGB(),
+        color: randomRGB(),
+        marginTop: `${Math.random()}px`,
+      }),
+    );
+  }
+
+  return (
+    <GlitzProvider glitz={glitz}>
+      {components.map((Component, i) => (
+        <Component key={i} />
+      ))}
+    </GlitzProvider>
+  );
+}
