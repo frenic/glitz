05ddea8a19866e068bc7074019fe3f25c4e52e79
Anders Ekdahl <anders.ekdahl@avensia.com>
2020-10-11 08:27:42 +0200

    Include static-react.ts in bundle

--- packages/static/post-bundle.js
+++ packages/static/post-bundle.js
@@ -2,4 +2,5 @@ const fs = require('fs');
 const path = require('path');
 
 fs.copyFileSync(path.join(__dirname, 'src', 'static-glitz.ts'), path.join(__dirname, 'cjs', 'static-glitz.ts'));
+fs.copyFileSync(path.join(__dirname, 'src', 'static-react.ts'), path.join(__dirname, 'cjs', 'static-react.ts'));
 fs.copyFileSync(path.join(__dirname, 'src', 'shared.ts'), path.join(__dirname, 'cjs', 'shared.ts'));
