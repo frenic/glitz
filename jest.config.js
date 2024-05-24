51ef68da9ab977a604f55d867ac3c868486f45b5
Eric Lindell <eric.lindell@avensia.com>
2024-05-24 14:54:34 +0200

    chore: upgrade jest, ts-jest, jsdom, testing-library

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,4 @@
 module.exports = {
-  globals: {
-    'ts-jest': {
-      diagnostics: false,
-      tsconfig: {
-        target: 'es6',
-        module: 'commonjs',
-        sourceMap: true,
-        inlineSourceMap: true,
-      },
-    },
-  },
   moduleFileExtensions: ['ts', 'tsx', 'js'],
   moduleNameMapper: {
     '^@glitz/core$': '<rootDir>/packages/core/src',
@@ -17,7 +6,18 @@ module.exports = {
   },
   transformIgnorePatterns: ['node_modules'],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        diagnostics: false,
+        tsconfig: {
+          target: 'es6',
+          module: 'commonjs',
+          sourceMap: true,
+          inlineSourceMap: true,
+        },
+      },
+    ],
   },
   testMatch: ['**/src/**/!(typecheck).spec.(ts|tsx)'],
 };
